3aaa645d5b13a814088a9223e49511f2a25790a8
Carlos <85771477+penguinlambz@users.noreply.github.com>
2026-08-10 03:45:35 -0400

    rom header bits

--- asm/rom_header.s
+++ asm/rom_header.s
@@ -8,47 +8,17 @@
 .word  entry_point              /* Entrypoint */
 
 /* Revision */
-#ifdef VERSION_SH
-    .word  0x00001448
-#elif defined(VERSION_CN)
-    .word  0x0000144C
-#elif defined(VERSION_EU)
-    .word  0x00001446
-#else /* NTSC-U and NTSC-J 1.0 */
     .word  0x00001444
-#endif
-
-#ifdef VERSION_CN
-    .fill 0x30
-#else
 
 .word  0x4EAA3D0E               /* Checksum 1 */
 .word  0x74757C24               /* Checksum 2 */
 .word  0x00000000               /* Unknown */
 .word  0x00000000               /* Unknown */
-#ifdef VERSION_SH
-.ascii "SUPERMARIO64        "   /* Internal ROM name */
-#else
-.ascii "SUPER MARIO 64      "   /* Internal ROM name */
-#endif
+.ascii "96FLASHBACKS        "   /* Internal ROM name */
 .word  0x00000000               /* Unknown */
 .word  0x0000004E               /* Cartridge */
 .ascii "SM"                     /* Cartridge ID */
 
 /* Region */
-#ifdef VERSION_EU
-    .ascii "P"                  /* PAL (Europe) */
-#elif defined(VERSION_US)
-    .ascii "E"                  /* NTSC-U (North America) */
-#else
     .ascii "J"                  /* NTSC-J (Japan) */
-#endif
-
-
-#ifdef VERSION_SH
-    .byte  0x03                 /* Version (Shindou) */
-#else
     .byte  0x00                 /* Version */
-#endif
-
-#endif
