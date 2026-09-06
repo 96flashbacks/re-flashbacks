d6ef3a7efc062c21630bfc39c3be0b12f8f1ca2d
somepersononthenet <soupnuts6061@gmail.com>
2026-09-06 00:46:39 -0400

    Update makecontinuous.sh

--- makecontinuous.sh
+++ makecontinuous.sh
@@ -9,11 +9,3 @@ printf "$HASH_STAMP" | dd of=build/jp/sm64.jp.z64 bs=1 seek=42 count=7 conv=notr
 tools/sm64tools/n64cksum build/jp/sm64.jp.z64
 make patch -j
 cp build/jp/sm64.jp.bps "$1-$2.bps"
-
-# HLE Build
-make clean
-make -j GRUCODE=f3d_20E
-printf "$HASH_STAMP" | dd of=build/jp/sm64.jp.z64 bs=1 seek=42 count=7 conv=notrunc
-tools/sm64tools/n64cksum build/jp/sm64.jp.z64
-make patch -j GRUCODE=f3d_20E
-cp build/jp/sm64.jp.bps "$1-$2-hle.bps"
